1d3f12ec9950a631b5fcd9d45f487baa9c88276c
andreffs18 <andreffs18@gmail.com>
2017-07-21 23:42:24 +0100

    Add quotes.py and notebook to generate new ones

--- run_bot.py
+++ run_bot.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
+import re
 import time
 import random
+import string
 from datetime import datetime, timedelta
 
 from slackclient import SlackClient
+
+from quotes import QUOTES, TRIGGER_WORDS
 
 SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
 BOT_NAME = os.environ.get("BOT_NAME", 'santosbot')
@@ -13,33 +17,8 @@ BOT_ID = os.environ.get("BOT_ID")
 READ_WEBSOCKET_DELAY = 1
 # timeout before re-using the same quote
 QUOTE_REUSE_TIMEOUT = 15 # seconds
-
-QUOTES = {
-	0: u"O único preço da tua liberdade é a responsabilidade que tens por escolheres usá-la.",
-	1: u"Assim como os confiantes atraem os confiantes, os invejosos, rancorosos e maldosos também se atraem uns aos outros.",
-	2: u"Quem pensa por si, afirma o que sente e materializa as suas vontades, será sempre controverso, mas é precisamente da controvérsia que nasce a mudança, é do desacordo que se dá a evolução da alma.",
-	3: u"A liberdade é o expoente máximo do amor-próprio.",
-	4: u"Só aquele que ousa, por sua livre e espontânea vontade, abandonar o rebanho da indolência para fazer o seu próprio destino é que alcançará, verdadeiramente, o total poder da alma que é.",
-	5: u"Não te queixes da vida, não fiques à espera dela, cria-a, e terás a vida que sempre sonhaste.",
-	6: u"Uma pessoa que não sonha não encontra motivos, realmente fortes, para acordar todas as manhãs com vontade de viver, nãqo se conecta com a divindade em si mesma.",
-}
-
-TRIGGER_WORDS = {
-	u'liberdade': {'quotes': [0, 3], 'last_used_at': datetime.min},
-	u'responsabilidade': {'quotes': [0], 'last_used_at': datetime.min},
-	u'escolher': {'quotes': [0], 'last_used_at': datetime.min},
-	u'escolhas': {'quotes': [0], 'last_used_at': datetime.min},
-	u'escolha': {'quotes': [0], 'last_used_at': datetime.min},
-	u'confiança': {'quotes': [1], 'last_used_at': datetime.min},
-	u'confiante': {'quotes': [1], 'last_used_at': datetime.min},
-	u'atração': {'quotes': [1], 'last_used_at': datetime.min},
-	u'atrair': {'quotes': [1], 'last_used_at': datetime.min},
-	u'invejosa': {'quotes': [1], 'last_used_at': datetime.min},
-	u'invejoso': {'quotes': [1], 'last_used_at': datetime.min},
-	u'sentimos': {'quotes': [2], 'last_used_at': datetime.min},
-	u'sentimentos': {'quotes': [2], 'last_used_at': datetime.min},
-	u'amor': {'quotes': [3], 'last_used_at': datetime.min},
-}
+# MAKE GLOBAL VAR TO PREVENT DOUBLE POSTING
+LAST_POST_AT = datetime.min
 
 
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
@@ -56,6 +35,15 @@ def get_bot_id():
 	return bot_string.format(dict(members).get(BOT_NAME))
 
 
+def tokenize_string(text):
+	""""""
+	new_tokens = list()
+	regex = re.compile('[%s]' % re.escape(string.punctuation))
+	text = regex.sub('', text)
+	tokens = [t.strip().lower() for t in text.split()]
+	return filter(None, tokens)
+	
+
 def parse_slack_output(slack_rtm_output):
 	"""The Slack Real Time Messaging API is an events firehose. 
 	This parsing function returns None unless a message is directed
@@ -70,8 +58,7 @@ def parse_slack_output(slack_rtm_output):
 
 			print(datetime.now(), output_list)
 			
-			text = output.get('text', "")
-			tokenized_text = filter(None, map(lambda x: x.strip().lower(), text.split()))
+			tokenized_text = tokenize_string(output.get('text', ""))
 			intersection = set(TRIGGER_WORDS).intersection(tokenized_text)
 			
 			if intersection:
@@ -79,12 +66,14 @@ def parse_slack_output(slack_rtm_output):
 				quote_id = random.choice(TRIGGER_WORDS.get(word).get('quotes'))
 				quote = QUOTES.get(quote_id)
 				last_used_at = TRIGGER_WORDS.get(word).get('last_used_at')
-				if last_used_at + timedelta(seconds=QUOTE_REUSE_TIMEOUT) > datetime.now():
+				if last_used_at + timedelta(seconds=QUOTE_REUSE_TIMEOUT) > datetime.now() :
 					print(u"Skipping quote for \"{}\", last used {} and it will be available {}s after.".format(word, last_used_at, QUOTE_REUSE_TIMEOUT))
 					continue
 
 				trigger_word = TRIGGER_WORDS.get(word)
 				trigger_word.update({'last_used_at': datetime.now()})
+
+				LAST_POST_AT = datetime.now() 
 
 				print(u"> FOUND QUOTE: {} = {}: {}".format(word, quote_id, quote))
 				slack_client.api_call("chat.postMessage", channel=output['channel'], text=quote, as_user=True)
@@ -98,7 +87,11 @@ if __name__ == "__main__":
 	if slack_client.rtm_connect():
 		print("\"{}\" connected and running!".format(BOT_NAME))
 		while True:
+			if LAST_POST_AT + timedelta(seconds=QUOTE_REUSE_TIMEOUT) > datetime.now():
+				print("Preventing double posting: last post @ {}".format(LAST_POST_AT))
+				time.sleep(QUOTE_REUSE_TIMEOUT)
 			parse_slack_output(slack_client.rtm_read())
-			time.sleep(READ_WEBSOCKET_DELAY)
+			time.sleep(READ_WEBSOCKET_DELAY)			
+
 	else:
 		print("Connection failed. Invalid Slack token or bot ID?")
