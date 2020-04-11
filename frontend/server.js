e50956d4ac609b88b128c05d450c96f8a5a43924
andreffs18 <andreffs18@gmail.com>
2020-04-11 12:05:58 +0100

    ⚡️ Add express.js server to serve static assets

--- frontend/server.js
+++ frontend/server.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
+const express = require("express");
+const serveStatic = require("serve-static");
+const path = require("path");
 
-const app = express()
+const app = express();
 
 //here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
 // this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function (req, res) {
-    res.sendFile(path.join(__dirname, '/dist/index.html'))
-})
+app.get(/.*/, function(req, res) {
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
+});
 
-const port = process.env.PORT || 8080
-app.listen(port)
-console.log(`app is listening on port: ${port}`)
+const port = process.env.PORT || 8080;
+app.listen(port);
+console.log(`Server is listening on port: ${port}`);
